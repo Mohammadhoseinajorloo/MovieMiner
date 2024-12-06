a44ff502a946d6b77d79b9b0dc65f7e22975665e
mohammad <mohammadhoseinajorloo76@gmail.com>
2024-12-06 15:10:14 -0500

    add extractio module in extraction folder

---
+++ extraction/base.py
@@ -0,0 +1,88 @@
+import os
+import sys
+WORK_DIR = os.getcwd()
+sys.path.append(WORK_DIR)
+
+from article import Article
+
+from bs4 import BeautifulSoup
+import requests
+import datetime
+import jdatetime
+
+
+class BaseExtract:
+    def __init__(self, url):
+        self.url = url
+
+    def parse_url (self, url: str) -> str:
+        res = requests.get(url)
+        return res.text
+
+    def fetch_url(self, html: str) -> BeautifulSoup:
+        soup = BeautifulSoup(html, "html.parser")
+        return soup
+
+    def extract_articles(self, soup: BeautifulSoup) -> list:
+        articels_post_box = soup.find("div", class_="rt-posts right is-archive list-mode")
+        section_rt = articels_post_box.find("section", class_="rt") 
+        div_rt = section_rt.find_all("div", class_="rt")[1]
+        articels = div_rt.find_all("article")
+        return articels
+
+    def extract_info_articel(self, articels: list):
+        articles_list = []
+        today = datetime.datetime.now()
+        shamsi_date = shamsi_date = jdatetime.date.fromgregorian(date=today)
+        shamsi_day = shamsi_date.day
+        for article in articels:
+            update_time = self.extract_update_time(article)
+            if shamsi_day == update_time.day:
+                page_link = self.extracte_page_link(article)
+                title = self.extracte_title(article)
+                image = self.extracte_image(article) 
+                imdb_rate, vote_rate, user_satisfaction_rate, metacritic_rate = self.extracte_rates(article)
+                geners = self.extract_geners(article)
+                year_realese = self.extract_year_realese(article)
+                movie_time = self.extract_movie_time(article)
+                quality = self.extract_quality(article)
+                product = self.extract_product(article)
+                language = self.extract_language(article)
+                director = self.extract_director(article)
+                stars = self.extract_stars(article)
+                discription = self.extract_discription(article)
+                like = self.extract_like(article)
+                dislike = self.extract_dislike(article)
+                article_obj = Article(
+                        page_link,
+                        title,
+                        image,
+                        imdb_rate,
+                        vote_rate,
+                        user_satisfaction_rate,
+                        metacritic_rate,
+                        geners,
+                        year_realese,
+                        movie_time,
+                        quality,
+                        product,
+                        language,
+                        director,
+                        stars,
+                        update_time,
+                        discription,
+                        like,
+                        dislike
+                        )
+                articles_list.append(article_obj)
+            else:
+                continue
+
+        return articles_list
+
+    def scrape(self):
+        html = self.parse_url (self.url)
+        soup = self.fetch_url(html)
+        movies_list = self.extract_articles(soup)
+        movies_info = self.extract_info_articel(movies_list)
+        return movies_info
---
+++ extraction/film.py
@@ -0,0 +1,104 @@
+from .base import BaseExtract
+from updatetime import UpdateTime
+
+from bs4 import BeautifulSoup
+
+
+class FilmExtract(BaseExtract):
+    def extracte_page_link(self, soup: BeautifulSoup) -> str: 
+        return soup.find("div", class_="titr rt").find("h2", class_="title right rt-18").find("a")["href"]
+
+    def extracte_image(self, soup: BeautifulSoup) -> str: 
+        return soup.find("a", class_="right rt-relative photo").find("img", class_="owl-lazy right pic rt-blur rt-10px")["data-src"]
+     
+    def extracte_title(self, soup: BeautifulSoup) -> str: 
+        titles = soup.find("div", class_="titr rt").find("h2", class_="title right rt-18").text
+        titles = titles.split(" ")[2:]
+        for title in titles:
+            if title.isdigit() and int(title) > 1900:
+                titles.remove(title)
+        return " ".join(titles)
+
+    def extracte_rates(self, soup: BeautifulSoup) -> tuple[int, int, int]:
+        imdb = float(soup.find("ul", class_="info left rt-13 rt-relative rt-ddd").find("div", class_="emtiazs left rt-absolute rt-15px").find("a", class_="ins rt-ddd rt").find("div", class_="rt").find("span", class_="val-imdb left rt-18 rt-bold rt-dana").text.split("/")[1])
+        vote = float(soup.find("ul", class_="info left rt-13 rt-relative rt-ddd").find("div", class_="emtiazs left rt-absolute rt-15px").find("a", class_="ins rt-ddd rt").find("span", class_="val left rt-999 rt-bold rt-12").text.split(" ")[0].replace(",", ""))
+
+        try:
+            user_satisfaction = soup.find("ul", class_="info left rt-13 rt-relative rt-ddd").find("div", class_="emtiazs left rt-absolute rt-15px").find("span", class_="val-imdb left rt-18 rt-bold rt-dana isx_green").text
+            user_satisfaction = float(user_satisfaction.split('%')[0])
+        except:
+            user_satisfaction = None
+
+        try:
+            metacritic = int(soup.find("ul", class_="info left rt-13 rt-relative rt-ddd").find("div", class_="ins rt").find("div", class_="rt").find("span", class_="val-imdb left rt-18 rt-bold rt-dana").text)
+        except:
+            metacritic = None
+
+        return imdb, vote, user_satisfaction, metacritic
+
+    def extract_geners(self, soup: BeautifulSoup) -> list:
+        geners_list = []
+        geners_part = soup.find("ul", class_="info left rt-13 rt-relative rt-ddd").find_all("li", class_="rt")[0]
+        geners = geners_part.find("span", class_="value right rt-fff").find_all("a")
+        for gener in geners:
+            geners_list.append(gener.text)
+        return geners_list
+
+    def extract_year_realese(self, soup: BeautifulSoup) -> int:
+        year_realese_part = soup.find("ul", class_="info left rt-13 rt-relative rt-ddd").find_all("li", class_="rt")[1]
+        year_realese = soup.find_all("span", class_="value right rt-fff")[1].find("a").text
+        return int(year_realese)
+
+    def extract_movie_time(self, soup: BeautifulSoup) -> str:
+        try:
+            movie_time_part = soup.find("ul", class_="info left rt-13 rt-relative rt-ddd").find_all("li", class_="rt")[2]
+            movie_time = movie_time_part.find("span", class_="value right rt-fff").text
+            split_movie_time_list = movie_time.strip() .split(" ")
+            houre = int(split_movie_time_list[0])
+            minute = int(split_movie_time_list[3])
+            movie_time = houre * 60 + minute
+        except: 
+            movie_time = None
+        return movie_time
+        
+    def extract_quality(self, soup: BeautifulSoup) -> str:
+        quality_part = soup.find("ul", class_="info left rt-13 rt-relative rt-ddd").find_all("li", class_="rt")[3]
+        quality = quality_part.find("span", class_="value right rt-fff").text
+        return quality
+
+    def extract_product(self, soup: BeautifulSoup) -> str:
+        product_part = soup.find("ul", class_="info left rt-13 rt-relative rt-ddd").find_all("li", class_="rt")[4]
+        product = product_part.find("span", class_="value right rt-fff").text
+        return product
+
+    def extract_language(self, soup: BeautifulSoup) -> str:
+        language_part = soup.find("ul", class_="info left rt-13 rt-relative rt-ddd").find_all("li", class_="rt")[5]
+        language = language_part.find("span", class_="value right rt-fff").text
+        return language 
+
+    def extract_director(self, soup: BeautifulSoup) -> str:
+        director_part = soup.find("ul", class_="info left rt-13 rt-relative rt-ddd").find_all("li", class_="rt")[6]
+        director = director_part.find("span", class_="value right rt-fff").text
+        return director 
+
+    def extract_stars(self, soup: BeautifulSoup) -> str:
+        stars_part = soup.find("ul", class_="info left rt-13 rt-relative rt-ddd").find_all("li", class_="rt")[7]
+        stars = stars_part.find("span", class_="value right rt-fff").text
+        return stars 
+
+    def extract_update_time(self, soup: BeautifulSoup) -> UpdateTime:
+        update_time_part = soup.find("ul", class_="info left rt-13 rt-relative rt-ddd").find_all("li", class_="rt")[8]
+        update_time = update_time_part.find("span", class_="value right rt-fff").text
+        return UpdateTime(update_time)
+
+    def extract_discription(self, soup: BeautifulSoup) -> str:
+        return soup.find("div", class_="text rt rt-ddd rt-13 rt-10px").find("p").text
+
+    def extract_like(self, soup: BeautifulSoup) -> str:
+        return int(soup.find("div", class_="like-dis").find("div", class_="mi-icons mi-like movie-like").find("span", class_="count").text)
+
+    def extract_like(self, soup: BeautifulSoup) -> str:
+        return int(soup.find("div", class_="like-dis").find("div", class_="mi-icons mi-like movie-like").find("span", class_="count").text)
+
+    def extract_dislike(self, soup: BeautifulSoup) -> str:
+        return int(soup.find("div", class_="mi-icons mi-dislike movie-dislike").find("span", class_="count").text)
