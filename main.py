587110c1e55ef964ff0baa3210ecdda1bc735661
mohammad <mohammadhoseinajorloo76@gmail.com>
2024-12-07 08:54:43 -0500

    add sqlite in main file

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from extraction.film import FilmExtract
 import jdatetime
+import sqlite3
 
 FILM_URL = 'https://golchindls.ir/movie_cats/film/'
 
 def main():
+    print(sqlite3.sqlite_version)
+    """
     page = 0
     while True:
         url = FILM_URL+"page/"+str(page)+"/" 
@@ -17,6 +20,7 @@ def main():
             print(movie)
         print("_______________________________________________________________")
         page += 1
+    """
     
 if __name__ == "__main__":
     main()
