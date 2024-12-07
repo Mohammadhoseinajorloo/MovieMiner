66d7cef016d87a9384b4f4b9b68b99d14c27ee6e
mohammad <mohammadhoseinajorloo76@gmail.com>
2024-12-07 09:20:01 -0500

    add connection database in main file

--- main.py
+++ main.py
@@ -3,9 +3,31 @@ import jdatetime
 import sqlite3
 
 FILM_URL = 'https://golchindls.ir/movie_cats/film/'
+DATABASE_ADDRESS = "movies.db"
+
+
+class DataBaseHandler:
+
+    def __init__(self, db_address):
+        try:
+            self.connection = sqlite3.connect(db_address)
+            self.cursor = self.connection.cursor()
+            print(f"SQLITE Connection Established!!!")
+            self.cursor.close()
+        except sqlite3.Error as error:
+            print(f"Error while connection to sqlite", error)
+        finally:
+            if (self.connection):
+                self.connection.close()
+                print("connection closed...")
+
+    def create_table(self, tabel_name):
+        pass
+    
+
 
 def main():
-    print(sqlite3.sqlite_version)
+    db = DataBaseHandler(DATABASE_ADDRESS)
     """
     page = 0
     while True:
@@ -21,6 +43,6 @@ def main():
         print("_______________________________________________________________")
         page += 1
     """
-    
+
 if __name__ == "__main__":
     main()
