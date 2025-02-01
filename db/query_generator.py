413014add92ff94c5be5303299c9c9e891b8171d
mohammad <mohammadhoseinajorloo76@gmail.com>
2025-02-01 17:19:08 +0330

    Fix bug in insert function on database part

--- db/query_generator.py
+++ db/query_generator.py
@@ -6,6 +6,8 @@ sys.path.append(WORK_DIR)
 from article import Article
 from logger import log_execution
 import sqlite3
+import mysql.connector
+
 
 class QueryGenerator:
 
@@ -14,18 +16,18 @@ class QueryGenerator:
         fields = article.get_filds()
         placeholders = ", ".join(f"`{field}` TEXT" for field in fields)
         if isinstance(connection, sqlite3.Connection):
-            return f"CREATE TABLE IF NOT EXISTS `{tabel_name}` (id INTEGER PRIMARY KEY AUTOINCREMENT,{placeholders})"
-        else:
+            return f"CREATE TABLE IF NOT EXISTS {tabel_name} (id INTEGER PRIMARY KEY AUTOINCREMENT,{placeholders})"
+        elif isinstance(connection, mysql.connector.MySQLConnection):
             return f"CREATE TABLE IF NOT EXISTS `{tabel_name}` (id INT PRIMARY KEY AUTO_INCREMENT, {placeholders})"
 
 
     @log_execution
     def insert_query(self, connection , tabel_name: str, article: Article):
         fields = article.get_filds()
-        field_names = ", ".join(fields)
+        field_names = ", ".join(f"`{field}`" for field in fields)
         if isinstance(connection, sqlite3.Connection):
             value_placeholders = ", ".join("?" for _ in fields)
-        else:
+        elif isinstance(connection, mysql.connector.MySQLConnection):
             value_placeholders = ", ".join("s%" for _ in fields)
-        return f"INSERT INTO `{tabel_name}` (`{field_names}`) VALUES ({value_placeholders})"
+        return f"INSERT INTO {tabel_name} ({field_names}) VALUES ({value_placeholders})"
 
