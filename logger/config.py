2ec7280ad38eb1100159f2b6c914ca5f5d4736c6
mohammad <mohammadhoseinajorloo76@gmail.com>
2025-02-04 21:43:17 +0330

    Fix bug not file exitst logs folder in logs part

--- logger/config.py
+++ logger/config.py
@@ -1,9 +1,12 @@
 import jdatetime
+import os
 
 
 LOGGING_FORMAT = '[%(levelname)s] %(asctime)s - (%(name)s/%(filename)s/%(funcName)s/%(lineno)d) - %(message)s'
 LOGGING_TIME_FORMAT = '%Y-%m-%d %H:%M:%S'
 today = jdatetime.date.today()
 current_month = f"{today.year}-{today.month:02d}"
+if not os.path.isdir("logger/logs"):
+    os.mkdir("logger/logs")
 LOG_FILE = f'logger/logs/logs_{current_month}.log'
 LOGGER_NAME = "DownloaderLogger"
