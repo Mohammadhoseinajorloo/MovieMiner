65941790d1efb06c931bee5e11db4797aaa89e41
mohammad <mohammadhoseinajorloo76@gmail.com>
2025-03-18 17:53:01 +0330

    Fix bug `cls` not difind in action database
    chenge `cls._instance.connection` and `cls._instance.cursor` with
    "self.connetion" and "self.cursor" fixed bug in create_table function
    and continu runing app

--- db/action.py
+++ db/action.py
@@ -24,9 +24,9 @@ class ActionDB(DBConnection):
     @log_execution
     def _isexist(self, tabel_name: str, article: Article):
         """ Action is exist item in database """
-        isexist_query = self.qg.isexist_query(cls._instance.connection, tabel_name, article)
-        cls._instance.cursor.execute(isexist_query)
-        awnser = cls._instance.cursor.fetchone()
+        isexist_query = self.qg.isexist_query(self.connection, tabel_name, article)
+        self.cursor.execute(isexist_query)
+        awnser = self.cursor.fetchone()
         if awnser is None:
             return False
         return True
@@ -35,25 +35,25 @@ class ActionDB(DBConnection):
     @log_execution
     def _update(self, tabel_name:str, article: Article):
         """ Update row data in database """
-        update_query = self.qg.update_query(cls._instance.connection, tabel_name, article)
-        cls._instance.cursor.execute(update_query, article.get_values())
-        cls._instance.connection.commit()
+        update_query = self.qg.update_query(self.connection, tabel_name, article)
+        self.cursor.execute(update_query, article.get_values())
+        self.connection.commit()
 
 
     @log_execution
     def create_table(self, tabel_name: str, article: Article):
         """ Action create table in database """
-        create_table_query = self.qg.create_table_query(cls._instance.connection, tabel_name, article)
-        cls._instance.cursor.execute(create_table_query)
+        create_table_query = self.qg.create_table_query(self.connection, tabel_name, article)
+        self.cursor.execute(create_table_query)
 
 
     @log_execution
     def insert(self, tabel_name: str, article: Article):
         """ Action insert item in database """
         if not self._isexist(tabel_name, article):
-            insert_query = self.qg.insert_query(cls._instance.connection, tabel_name, article)
-            cls._instance.cursor.execute(insert_query, article.get_values())
-            cls._instance.connection.commit()
+            insert_query = self.qg.insert_query(self.connection, tabel_name, article)
+            self.cursor.execute(insert_query, article.get_values())
+            self.connection.commit()
         else:
             self._update(tabel_name, article)
             logger.warning(f"{article.filds['title']} movie exist in database")
