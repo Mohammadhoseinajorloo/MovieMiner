d5a7afac9591aed55d70ed1eab1db7f2c1f0ee6b
mohammad <mohammadhoseinajorloo76@gmail.com>
2025-03-20 16:40:12 +0330

    Add calculate file log in filelogger class
    for create log file for each month and save loggs in file spesific each
    month add calculate file log function in filelogger object.

--- logger/file_logger.py
+++ logger/file_logger.py
@@ -1,22 +1,32 @@
 from logging.handlers import TimedRotatingFileHandler
 from .base_logger import BaseLogger
+import jdatetime
 import logging
 import os
 
 
 class FileLogger(BaseLogger):
 
-    LOG_FILE = "logger/logs/app.log"
 
-    def __init__(self, log_file=LOG_FILE, log_level=logging.ERROR):
+    def __init__(self, log_level=logging.DEBUG):
         if not hasattr(self, "initialized"):
             super().__init__(log_level)
+            log_file = self.calculate_log_file()
             os.makedirs(os.path.dirname(log_file), exist_ok=True)
             file_handler = TimedRotatingFileHandler(log_file, when="midnight", interval=1, backupCount=30)
             file_handler.setLevel(log_level)
             file_handler.setFormatter(logging.Formatter("[%(levelname)s] %(asctime)s - %(message)s"))
             self.logger.addHandler(file_handler)
             self.initialized = True
+
+
+    def calculate_log_file(self):
+       today = jdatetime.date.today()
+       log_format = ".log"
+       log_location = "logger/logs" 
+       current_month = f"logs_{today.year}-{today.month:02d}"
+       log_file = f"{log_location}/{current_month}{log_format}"
+       return log_file
 
 
 file_logger = FileLogger().get_logger()
